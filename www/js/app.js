4b3be8c7bbbec99ddeb6cf18b94c49e20462b9a3
Donald Hobbs <donald@graymata.com>
2014-10-30 21:04:08 +0200

    added pouchdb

--- www/js/app.js
+++ www/js/app.js
@@ -6,65 +6,86 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic',
-                           'starter.controllers',
-                           'starter.services'])
+    'starter.controllers',
+    'starter.services'
+])
 
-    .run(function($ionicPlatform) {
-        $ionicPlatform.ready(function() {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-//            if(window.cordova && window.cordova.plugins.Keyboard) {
-//                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-//            }
-//            if(window.StatusBar) {
-//                // org.apache.cordova.statusbar required
-//                StatusBar.styleDefault();
-//            }
-            if (!!navigator.splashscreen) {
-                navigator.splashscreen.hide();
-            }
-            
-        });
-    })
-
-    .config(function($stateProvider, $urlRouterProvider) {
-
-        // Ionic uses AngularUI Router which uses the concept of states
-        // Learn more here: https://github.com/angular-ui/ui-router
-        // Set up the various states which the app can be in.
-        // Each state's controller can be found in controllers.js
-        $stateProvider
-
-        // setup an abstract state for the tabs directive
-            .state('tab', {
-                url: "/tab",
-                abstract: true,
-                templateUrl: "templates/tabs.html"
-            })
-
-        // Each tab has its own nav history stack:
-            .state('tab.scan', {
-                url: '/scan',
-                views: {
-                    'tab-scan': {
-                        templateUrl: 'templates/tab-scan.html',
-                        controller: 'ScanCtrl'
-                    }
-                }
-            })
-
-            .state('tab.about', {
-                url: '/about',
-                views: {
-                    'tab-about': {
-                        templateUrl: 'templates/tab-about.html',
-                        controller: 'AboutCtrl'
-                    }
-                }
-            })
-
-        // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/tab/scan');
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        //            if(window.cordova && window.cordova.plugins.Keyboard) {
+        //                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        //            }
+        //            if(window.StatusBar) {
+        //                // org.apache.cordova.statusbar required
+        //                StatusBar.styleDefault();
+        //            }
+        if (!!navigator.splashscreen) {
+            navigator.splashscreen.hide();
+        }
 
     });
+})
 
+.config(function($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+    })
+
+    // Each tab has its own nav history stack:
+    .state('tab.lots', {
+        url: '/lots',
+        views: {
+            'tab-lots': {
+                templateUrl: 'templates/tab-lots.html',
+                controller: 'LotsCtrl'
+            }
+        }
+    })
+
+    .state('tab.scan', {
+        url: '/scan',
+        views: {
+            'tab-scan': {
+                templateUrl: 'templates/tab-scan.html',
+                controller: 'ScanCtrl'
+            }
+        }
+    })
+
+    .state('tab.about', {
+        url: '/about',
+        views: {
+            'tab-about': {
+                templateUrl: 'templates/tab-about.html',
+                controller: 'AboutCtrl'
+            }
+        }
+    })
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/scan');
+
+}).factory('todoDb', function() {
+    // PouchDB.destroy('kitftmobile', function(err, info) {
+    //     console.log(err);
+    //     console.log(info)
+    // });
+
+
+
+
+    var db = new PouchDB('kitftmobile');
+    return db;
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,69 @@
 angular.module('starter.controllers', [])
 
+.controller('LotsCtrl', function($scope, todoDb, $rootScope) {
+
+    $scope.lots = [];
+    $scope.online = false;
+    $scope.syncstatus = "Offline";
+    $scope.toggleOnline = function() {
+        $scope.online = !$scope.online;
+        if ($scope.online) { // Read http://pouchdb.com/api.html#sync
+            $scope.syncstatus = "Online";
+            $scope.sync = todoDb.sync('http://192.168.100.220:5984/gavelmate', {
+                    live: true
+                })
+                .on('error', function(err) {
+                    console.log("Syncing stopped");
+
+                    $scope.$apply(function() {
+                        $scope.syncstatus = err.statusText;
+                    });
+                    console.log(err);
+                });
+        } else {
+            $scope.sync.cancel();
+            $scope.syncstatus = "Offline";
+        }
+    };
+
+    todoDb.changes({
+        since: 'now',
+        live: true,
+        onChange: function(change) {
+            console.log(change);
+            todoDb.get(change.id, function(err, doc) {
+                if (err) console.log(err);
+                console.log(doc);
+                if (doc.type === 'grv_item') {
+                    $scope.$apply(function() {
+                        $scope.lots.push(doc);
+                    });
+                }
+
+            })
+
+        }
+    });
+
+    todoDb.query('receiving/get_all_lot_numbers', function(err, response) {
+        console.log(err);
+        console.log(response);
+        if (typeof response != 'undefined') {
+            var result = response.rows;
+            for (var i = 0; i < result.length; i++) {
+                $scope.lots.push(result[i].value);
+            }
+            console.log($rootScope.lots)
+        }
+        $scope.$apply(function() {
+            $scope.lots = $scope.lots;
+        });
+
+    });
+
+
+})
+
 .controller('ScanCtrl', function($scope, appServices) {
     $scope.message = '';
     $scope.click = function() {
@@ -16,8 +80,7 @@ angular.module('starter.controllers', [])
                         '<tr><td>Text:</td><td>&nbsp;</td><td>' + result.result.cancelled + '</td></tr>' +
                         '</tbody>' +
                         '</table>';
-                }
-                else {
+                } else {
                     $scope.message = '<b>ERROR</b>: ' + result;
                 }
             },
@@ -34,6 +97,28 @@ angular.module('starter.controllers', [])
     }
 })
 
-.controller('AboutCtrl', function($scope) {
-})
+.controller('AboutCtrl', function($scope, todoDb, $rootScope) {
+    $scope.online = false;
+    $scope.syncstatus = "Offline";
+    $scope.toggleOnline = function() {
+        $scope.online = !$scope.online;
+        if ($scope.online) { // Read http://pouchdb.com/api.html#sync
+            $scope.syncstatus = "Online";
+            $scope.sync = todoDb.sync('http://192.168.100.220:5984/gavelmate', {
+                    live: true
+                })
+                .on('error', function(err) {
+                    console.log("Syncing stopped");
 
+                    $scope.$apply(function() {
+                        $scope.syncstatus = err.statusText;
+                    });
+                    console.log(err);
+                });
+        } else {
+            $scope.sync.cancel();
+            $scope.syncstatus = "Offline";
+        }
+    };
+
+})
